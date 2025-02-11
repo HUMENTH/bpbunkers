481e174c4f4d98a19e57afc497a26e3b3bbba282
Himanshu Shivhare <himanshushivhare047@mail.com>
2025-02-11 11:32:34 +0530

    test

---
+++ bpbunkers/bp_bunkers/doctype/web/web.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Octavision Software Solutions and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+
+class Web(WebsiteGenerator):
+	pass
